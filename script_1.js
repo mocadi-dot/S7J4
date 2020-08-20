989d3d452cc8cffe6330a61226ce7ac7913253f2
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-08-21 01:45:18 +0200

    Update script_1.js

--- script_1.js
+++ script_1.js
@@ -1,1 +1,8 @@
-console.log("Bonjour monde");
+function hello() {
+  console.log("Bonjour");
+}
+
+function hiUser() {
+  let name = prompt("Quel est ton nom ?");
+  console.log("Bonjour " + name);
+}
