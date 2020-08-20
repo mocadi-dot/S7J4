5c2214e9a1676b2e33bed91215a675636b86d098
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-08-21 01:39:37 +0200

    Update script_4.js

--- script_4.js
+++ script_4.js
@@ -1,35 +1,49 @@
 // 2.3. Analyses de données
 // 2.3.1. Startup Nation
 function entrepreneurs() {
-const entrepreneurs = [
-  { first: 'Steve', last: 'Jobs', year: 1955 },
-  { first: 'Oprah', last: 'Winfrey', year: 1954 },
-  { first: 'Bill', last: 'Gates', year: 1955 },
-  { first: 'Sheryl', last: 'Sandberg', year: 1969 },
-  { first: 'Mark', last: 'Zuckerberg', year: 1984 },
-  { first: 'Beyonce', last: 'Knowles', year: 1981 },
-  { first: 'Jeff', last: 'Bezos', year: 1964 },
-  { first: 'Diane', last: 'Hendricks', year: 1947 },
-  { first: 'Elon', last: 'Musk', year: 1971 },
-  { first: 'Marissa', last: 'Mayer', year: 1975 },
-  { first: 'Walt', last: 'Disney', year: 1901 },
-  { first: 'Larry', last: 'Page', year: 1973 },
-  { first: 'Jack', last: 'Dorsey', year: 1976 },
-  { first: 'Evan', last: 'Spiegel', year: 1990 },
-  { first: 'Brian', last: 'Chesky', year: 1981 },
-  { first: 'Travis', last: 'Kalanick', year: 1976 },
-  { first: 'Marc', last: 'Andreessen', year: 1971 },
-  { first: 'Peter', last: 'Thiel', year: 1967 }
+const a = [
+    { first: 'Steve', last: 'Jobs', year: 1955 },
+    { first: 'Oprah', last: 'Winfrey', year: 1954 },
+    { first: 'Bill', last: 'Gates', year: 1955 },
+    { first: 'Sheryl', last: 'Sandberg', year: 1969 },
+    { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+    { first: 'Beyonce', last: 'Knowles', year: 1981 },
+    { first: 'Jeff', last: 'Bezos', year: 1964 },
+    { first: 'Diane', last: 'Hendricks', year: 1947 },
+    { first: 'Elon', last: 'Musk', year: 1971 },
+    { first: 'Marissa', last: 'Mayer', year: 1975 },
+    { first: 'Walt', last: 'Disney', year: 1901 },
+    { first: 'Larry', last: 'Page', year: 1973 },
+    { first: 'Jack', last: 'Dorsey', year: 1976 },
+    { first: 'Evan', last: 'Spiegel', year: 1990 },
+    { first: 'Brian', last: 'Chesky', year: 1981 },
+    { first: 'Travis', last: 'Kalanick', year: 1976 },
+    { first: 'Marc', last: 'Andreessen', year: 1971 },
+    { first: 'Peter', last: 'Thiel', year: 1967 }
 ];
-//étape 1réalise un script qui filtrera les entrepreneurs nés dans les années 70
 
 
-//2 sors une array qui contient le prénom et le nom des entrepreneurs
+let res1 = [];
+let res2 = [];
+let res3 = [];
+let res4 = [];
 
-//3 quel âge aurait chaque inventeur aujourd'hui
+for (var i = 0; i < a.length; i++) {
+    if ((a[i].year > 1969) && (a[i].year < 1980)) {
+        res1.push(a[i]);
+    }
+    res2.push(a[i].first);
+    res2.push(a[i].last);
+    res3.push(2020 - a[i].year);
+    res4.push(a[i].last);
 
-//Trie les entrepreneurs par ordre alphabétique https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/sort
-  console.log("Les entrepreneurs par de nom de famille")
-  console.log(entrepreneurs.sort((a, b) => a.last.localeCompare(b.last)))
+
 }
-  
+console.log("######## ANNEE 70 ########");
+console.log(res1);
+console.log("######## Name ########");
+console.log(res2);
+console.log("######## AGE ########");
+console.log(res3);
+console.log("######## Tri alpha ########");
+console.log(res4.sort());
