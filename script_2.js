0b9ec496a3b1cd2ecaf34a044ce765cc483a04b4
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-08-21 01:41:29 +0200

    Update script_2.js

--- script_2.js
+++ script_2.js
@@ -2,7 +2,7 @@ prompt("De quel nombre veux-tu calculer la factorielle ?");
 
 // Utilisateur rentre un nombre (par ex 4)
 // Dans la console le résultat suivant est affiché
-"Le résultat est : 24"
+//"Le résultat est : 24"
 var f = [];
 function factorial (n) {
   if (n == 0 || n == 1)
