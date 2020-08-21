121c1d58fe6eb334844cc3febd42fd0b2fa5eaec
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-08-21 10:17:56 +0200

    Update script_3.js

--- script_3.js
+++ script_3.js
@@ -1,10 +1,19 @@
 // Pyramide de Mario
 // https://education.launchcode.org/skills-front-end/studios/mario/1/
 
-function pyramid() {
-  let n = prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?");
-  for (let i = 1; i <= n; i++) {
-    console.log(" ".repeat(n - i) + "#".repeat(i));
-  }
+number = prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?");
+
+function pyramide(nb) {
+	for (let count = 1; count <=nb; count++) {
+		let floor = '';
+		for ( let space = count; space <=nb; space++) {
+			floor += ' ';
+		};
+		for ( let wall = 1; wall <=count; wall++) {
+			floor += '#';
+		};
+		console.log(floor)
+	}
 }
-pyramid();
+
+pyramide(number);
